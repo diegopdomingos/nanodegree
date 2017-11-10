33d2e3c25ddc9f6c8b37f0c6aedbab1d7700459b
diegopdomingos <diego.pdomingos@gmail.com>
2017-11-09 22:30:44 -0200

    Update and rename Training.py to model.py
    - Add some comments

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -1,9 +1,5 @@
 
 # coding: utf-8
-
-# In[21]:
-
-
 import cv2
 import csv
 import numpy as np
@@ -18,17 +14,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 
-# In[22]:
-
-
+# First, we read the driving_log line by line which contains the name of the file of each
+# training image (left, center and right) and append it to a list
 lines = []
 with open("./data/driving_log.csv") as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
-                
+
+# Split the dataset into train and validation with factor of 0.2
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
+# Here we define our generator, so we dont use memory for all images in the same time
+# Instead, we use the yield function to return batches of images, avoiding high use
+# of memory
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
@@ -40,9 +39,10 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
                 
-                # For linux
+                # Images address for linux path
                 if batch_sample[0][0] == '/':
                     name = './data/IMG/'+batch_sample[0].split('/')[-1]
+                # Images address for Windows path
                 else:
                     name = './data/IMG/'+batch_sample[0].split('\\')[-1]
                 center_image = cv2.imread(name)
@@ -51,10 +51,10 @@ def generator(samples, batch_size=32):
                 angles.append(center_angle)
                 
                 # Flip image
-                #image_flipped = np.fliplr(center_image)
-                #measurement_flipped = -center_angle
-                #images.append(image_flipped)
-                #angles.append(measurement_flipped)
+                image_flipped = np.fliplr(center_image)
+                measurement_flipped = -center_angle
+                images.append(image_flipped)
+                angles.append(measurement_flipped)
                 
                 # For linux (left)
                 if batch_sample[1][0] == '/':
@@ -67,10 +67,10 @@ def generator(samples, batch_size=32):
                 angles.append(left_angle+0.1)
                 
                 # Flip image
-                #image_flipped = np.fliplr(left_image)
-                #measurement_flipped = -(left_angle+0.8)
-                #images.append(image_flipped)
-                #angles.append(measurement_flipped)
+                image_flipped = np.fliplr(left_image)
+                measurement_flipped = -(left_angle+0.8)
+                images.append(image_flipped)
+                angles.append(measurement_flipped)
                 
                 # For linux (right)
                 if batch_sample[2][0] == '/':
@@ -83,20 +83,18 @@ def generator(samples, batch_size=32):
                 angles.append(right_angle-0.1)
                 
                 # Flip image
-                #image_flipped = np.fliplr(right_image)
-                #measurement_flipped = -(right_angle-0.8)
-                #images.append(image_flipped)
-                #angles.append(measurement_flipped)
+                image_flipped = np.fliplr(right_image)
+                measurement_flipped = -(right_angle-0.8)
+                images.append(image_flipped)
+                angles.append(measurement_flipped)
 
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield shuffle(X_train, y_train)
 
-
-# In[23]:
-
-
+# Defining the model based on:
+# https://devblogs.nvidia.com/parallelforall/deep-learning-self-driving-cars/
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
@@ -108,8 +106,8 @@ model.add(Convolution2D(36,(5,5),activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(48,(3,3),activation="relu"))
 model.add(MaxPooling2D())
-#model.add(Convolution2D(64,(3,3),activation="relu"))
-#model.add(MaxPooling2D())
+#model.add(Convolution2D(64,(3,3),activation="relu")) # Because the MaxPooling and the size of the images
+#model.add(MaxPooling2D())                            # we get images with size <0
 model.add(Flatten())
 model.add(Dense(1164))
 model.add(Dense(100))
@@ -117,13 +115,14 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+# Define our BATCH_SIZE (this was choosen by trial and error until it train faster)
 BATCH_SIZE = 200
 
 train_generator = generator(train_samples, batch_size=BATCH_SIZE)
 validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
 
+# Compile and fit (train) our model
 model.compile(loss="mse", optimizer="Adam")
-#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
 model.fit_generator(train_generator, steps_per_epoch=             len(train_samples)/BATCH_SIZE+1, validation_data=validation_generator,             validation_steps=len(validation_samples)/BATCH_SIZE+1, epochs=2)
-model.save("model.h5")
+model.save("model.h5") # Save the model
 
